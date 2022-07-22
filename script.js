af9c6fc8a279a9e7c681b391760b2cd5fd76d024
Emily Santos <eyssantos@gmail.com>
2022-07-22 16:55:41 +0100

    add loader css and logic

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden =true;
+}
+
+// Hide Loading
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show New Quote
 const newQuote = () => {
+  loading();
   // Pick a random quote from apiQuotes array 
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if Author field is black and replace it with 'Unknown'
@@ -20,12 +34,15 @@ const newQuote = () => {
   if (quote.text.length > 120) {
     quoteText.classList.add('long-quote')
   } else {
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
   } 
 }
 
 // Get Quotes From API
 const getQuotes = async () => {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
